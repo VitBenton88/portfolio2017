f6e3051b72828211ef96ddafaf296ea49a3e0c18
Vit Benton <viteksk8alot@gmail.com>
2018-04-14 23:52:18 -0400

    Added postinstall script to paclage.json to run gulp js/sass compilements, seperated dev/production gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -19,24 +19,10 @@ const paths = {
   sassSrc: 'src/sass/style.scss'
 };
 
-// gulp.tasks
+// DEVELOPMENT gulp.tasks
 
-// minify & concat custom js
-gulp.task('scripts', (cb) => {
-  pump([
-      gulp.src(paths.scripts),
-      sourcemaps.init(),
-      concat('all.min.js'),
-      uglifyjs(),
-      sourcemaps.write(paths.root),
-      gulp.dest(paths.src)
-    ],
-    cb
-  );
-});
-
-// combine js libraries
-gulp.task('libraries', (cb) => {
+// combine js libraries - DEVELOPMENT
+gulp.task('libraries-development', (cb) => {
   pump([
       gulp.src(paths.libSrc),
       sourcemaps.init(),
@@ -48,16 +34,63 @@ gulp.task('libraries', (cb) => {
   );
 });
 
-//compile sass
-gulp.task('sass', () => {
+// concat custom js - DEVELOPMENT
+gulp.task('scripts-development', (cb) => {
+  pump([
+      gulp.src(paths.scripts),
+      sourcemaps.init(),
+      concat('all.min.js'),
+      sourcemaps.write(paths.root),
+      gulp.dest(paths.src)
+    ],
+    cb
+  );
+});
+
+//compile sass - DEVELOPMENT
+gulp.task('sass-development', () => {
   gulp.src(paths.sassSrc)
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(sourcemaps.write(paths.root))
+    .pipe(gulp.dest(paths.src));
+});
+
+// PRODUCTION gulp.tasks
+
+// combine js libraries - PRODUCTION
+gulp.task('libraries-production', (cb) => {
+  pump([
+      gulp.src(paths.libSrc),
+      concat('lib.min.js'),
+      gulp.dest(paths.src)
+    ],
+    cb
+  );
+});
+
+// minify & concat custom js - PRODUCTION
+gulp.task('scripts-production', (cb) => {
+  pump([
+      gulp.src(paths.scripts),
+      concat('all.min.js'),
+      uglifyjs(),
+      gulp.dest(paths.src)
+    ],
+    cb
+  );
+});
+
+//compile sass - PRODUCTION
+gulp.task('sass-production', () => {
+  gulp.src(paths.sassSrc)
     .pipe(autoprefixer())
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(concat('style.css'))
-    .pipe(sourcemaps.write(paths.root))
     .pipe(gulp.dest(paths.src));
 });
 
@@ -66,10 +99,13 @@ gulp.task('watch', () => {
   //live reloader
   livereload.listen();
   //watch for changes in js
-  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.scripts, ['scripts-development']);
   //watch for changes in sass
-  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.sass, ['sass-development']);
 });
 
 //define default gulp task
-gulp.task('default', ['libraries', 'scripts', 'sass']);
+gulp.task('default', ['libraries-development', 'scripts-development', 'sass-development']);
+
+//define default gulp task
+gulp.task('production', ['libraries-production', 'scripts-production', 'sass-production']);
